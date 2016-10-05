5759a00f133ec691558baf6e155e49068c34f24b
Maciej Adamski <maciej.adamski89@gmail.com>
2016-10-05 11:47:00 +0200

    delete

--- .vim/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-BUNDLE_DIRECTORY="~/.vim/bundle";
-GIT_CLONE=$(git clone);
-
-
-git clone https://github.com/VundleVim/Vundle.vim.git $BUNDLE_DIRECTORY/Vundle.vim
-git clone https://github.com/tpope/vim-surround.git $BUNDLE_DIRECTORY/vim-surround
-git clone https://github.com/vim-airline/vim-airline $BUNDLE_DIRECTORY/vim-airline
